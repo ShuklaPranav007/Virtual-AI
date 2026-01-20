5921618e565f046fd76fed9bcff5f412a36bfab5
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-01-20 14:49:00 +0530

    authorization routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/db.js"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.routes.js"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
 
-app.get("/",(req,res)=>{
-    res.send("hii")
-})
+
 
 app.listen(port,()=>{
     connectDb()
